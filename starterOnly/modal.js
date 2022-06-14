395c003acc5958a705346758673fec74053ebc2c
Marine P <72661857+Damedesang@users.noreply.github.com>
2022-06-14 17:34:32 +0200

    Formulaire
    Finalisation de mise en place du formulaire et de la conservation des donnée dans le formulaire en cas d'echec de validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,10 +20,41 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+// Validation ou non du formulaire
 function validationFormulaire(){
-  validationRadioBox();
+  if (validationRadioBox()){
+    alert('Merci');
+    document.getElementById("formulaire").submit;
+    //validé l'envoi et affiché message d'envoir reussi.
+  }
+  else {
+    alert('Vous devez choisir une option.');
+    //document.getElementById("boutonEnvoi").onsubmit="return false;";
+    //Ne pas validé l'envoi
+  }
   
 }
 function validationRadioBox(){
-  
+  if (document.getElementById("location1").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location2").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location3").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location4").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location5").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location6").checked == true){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
